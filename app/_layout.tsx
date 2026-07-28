59b334fc8042f8ccdbc77f8978f1fcdaad2d5356
sude <sudee.guzeel@gmail.com>
2026-07-28 15:13:07 +0300

    kayıt ol ve onboardinge yönlendirme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout() {
         <Stack screenOptions={{ headerShown: false }}>
           <Stack.Screen name="index" />
           <Stack.Screen name="login" />
+          <Stack.Screen name="register" />
           <Stack.Screen name="forgot-password" />
           <Stack.Screen name="email-sent" />
           <Stack.Screen name="onboarding/personal-info" />
